78286184d669c609a6a7fc76967f8e0fb9533b21
Kaan Efe Atalay <kaanefeatalay@gmail.com>
2025-10-21 23:35:19 +0300

    added docker-compose.yml

--- src/main/java/com/atalaykaan/spring_boot_redis_cache/service/ProductService.java
+++ src/main/java/com/atalaykaan/spring_boot_redis_cache/service/ProductService.java
@@ -13,7 +13,6 @@ import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
